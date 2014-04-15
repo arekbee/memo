1e3071bf26f26a4a29c8209a36a0e4367b1669f7
oczko <oczko74@op.pl>
2014-04-15 22:58:24 +0200

    baza

---
+++ memo/App_Data/testoweDane.sql
@@ -0,0 +1,5 @@
+﻿insert into rola(nazwa) values ('administrator'), ('zwykły');
+insert into slowko(eng,pl) values ('cat','kot'), ('dog','pies'), ('parrot','papuga'), ('elephant','słoń'), ('giraffe','żyrafa');
+insert into ustawieniaZagadki(opis) values ('Z języka polskiego na angielski'), ('Z języka angielskiego na polski');
+insert into uzytkownik(nazwa, haslo, rola, ustawienia) values ('admin','test', 1, 2), ('uzytkownik','1234', 2, 1);
+insert into statystykaUzytkownika(nazwa) values ('admin'), ('uzytkownik');
