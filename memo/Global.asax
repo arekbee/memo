81fa94d590a085c63b548348451f9bf39c799d02
oczko <oczko74@op.pl>
2014-04-15 15:15:40 +0200

    memo

---
+++ memo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="memo.MvcApplication" Language="C#" %>
---
+++ memo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(memo.Startup))]
+namespace memo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
