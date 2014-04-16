deb6696d0ef4960962acbcd5d5926bf4c247c4d2
paweltryba <trybap@gmail.com>
2014-04-16 11:06:14 +0200

    pobranieOpisuZBazy

---
+++ memo/Controllers/SlowkoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using memo.Models;
+
+namespace memo.Controllers
+{
+    public class SlowkoController : Controller
+    {
+        private bazaEntities db = new bazaEntities();
+        public ActionResult Index()
+        {
+
+            //string wynik = db.slowko.Where(x => x.pl == "pies").ToList().ElementAt(0).eng;
+            string wynik = db.uzytkownik.Where(x => x.nazwa == "admin").ToList().ElementAt(0).ustawieniaZagadki.opis;
+
+            ViewBag.Pytanie = wynik;
+
+            return View();
+        }
+	}
+}
