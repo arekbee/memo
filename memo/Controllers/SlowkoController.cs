cc04dd6277e66eed64d7890c0c24a006b0950d30
paweltryba <trybap@gmail.com>
2014-04-23 20:55:16 +0200

    Częsc Systemu Rejestracji/Logowania

--- memo/Controllers/SlowkoController.cs
+++ memo/Controllers/SlowkoController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using memo.Models;
 
@@ -9,16 +6,109 @@ namespace memo.Controllers
 {
     public class SlowkoController : Controller
     {
+        private int zwykly = 2;
+        private int administrator = 1;
+
+        private int angPol = 2;
+        private int polAng = 1;
+
         private bazaEntities db = new bazaEntities();
+
+        [HttpGet]
         public ActionResult Index()
         {
 
             //string wynik = db.slowko.Where(x => x.pl == "pies").ToList().ElementAt(0).eng;
-            string wynik = db.uzytkownik.Where(x => x.nazwa == "admin").ToList().ElementAt(0).ustawieniaZagadki.opis;
+            //string wynik = db.uzytkownik.Where(x => x.nazwa == "admin").ToList().ElementAt(0).ustawieniaZagadki.opis;
+            /*
+            uzytkownik nowy = new uzytkownik { nazwa = "u1", haslo = "1234", rola = 2, ustawienia = 1 };
+            db.uzytkownik.Add(nowy);
+            db.SaveChanges();
 
-            ViewBag.Pytanie = wynik;
+            ViewBag.Pytanie = wynik;*/
 
+            //RejestracjaModel nowy = new RejestracjaModel { nazwa = "u2", haslo = "1234", hasloPowtorka = "1234", regulamin = true };
+            //Rejestracja(nowy);
+
+
+            return RedirectToAction("Zaloguj");
+        }
+
+
+        [HttpGet]
+        public ActionResult Rejestracja()
+        {
             return View();
         }
+
+
+        
+        [HttpPost]
+        public ActionResult Rejestracja(RejestracjaModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var czyIstnieje = db.uzytkownik.Where(x => x.nazwa == model.nazwa).Count();
+                if(czyIstnieje > 0)
+                {
+                    ViewBag.Uzytkownik = "Użytkownik o podanym loginie już istnieje!";
+                    return View(model);
+                }
+
+                uzytkownik nUzytkownik = new uzytkownik { nazwa = model.nazwa.Trim(), haslo = model.haslo.Trim(), rola = zwykly, ustawienia = polAng };
+                db.uzytkownik.Add(nUzytkownik);
+
+
+                statystykaUzytkownika nStatystyka = new statystykaUzytkownika { nazwa = model.nazwa.Trim(), dobreOdpowiedzi = 0, zleOdpowiedzi = 0 };
+                db.statystykaUzytkownika.Add(nStatystyka);
+                db.SaveChanges();
+            }
+
+            return View("Zaloguj");
+        }
+
+
+        [HttpGet]
+        public ActionResult Zaloguj()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Zaloguj(LogowanieModel model)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                var user = db.uzytkownik.Where(x => x.nazwa == model.nazwa.Trim());
+                int czyIstnieje = user.Count();
+                if (czyIstnieje > 0) //istnieje użytkownik
+                {
+                    string haslo = user.First().haslo.ToString().Trim();
+                    string hasloUzytkownika = model.haslo.ToString().Trim();
+
+                    if (haslo.Equals(hasloUzytkownika)) //poprawnie zalogowany użytkownik
+                    {
+                        ViewBag.Login = user.First().nazwa.Trim();
+                        ViewBag.WiadLogowanie = "Zalogowano";
+                        return View("Index");
+                    }
+                }
+                ViewBag.WiadLogowanie = "Podałeś zły login lub hasło.";
+            }
+            else
+            {
+                ViewBag.WiadLogowanie = "Nie wpisałeś loginu lub hasła.";
+            }
+
+            return View(model);
+        }
+
+
+
+
+
+
+
 	}
 }
---
+++ memo/Models/Konto.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace memo.Models
+{
+    public class RejestracjaModel
+    {
+        [Required]
+        [Display(Name = "Nazwa użytkownika")]
+        public string nazwa { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} musi zawierać co najmniej następującą liczbę znaków: {2}.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string haslo { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("haslo", ErrorMessage = "Hasło i jego potwierdzenie są niezgodne.")]
+        public string hasloPowtorka { get; set; }
+
+        [Display(Name = "Akceptacja regulaminu")]
+        [Compare("regulamin", ErrorMessage = "Musisz zatwierdzić regulamin")]
+        public bool regulamin { get; set; }
+    }
+
+    public class LogowanieModel
+    {
+        [Required]
+        [Display(Name = "Nazwa użytkownika")]
+        public string nazwa { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string haslo { get; set; }
+    }
+
+
+
+
+
+
+
+}
