c4e68fba0a6a710b7b0e543dac8bde5cca3c73c7
oczko <oczko74@op.pl>
2014-05-15 15:03:07 +0200

    małe zmiany wyglądu

---
+++ memo/Scripts/MenuScript.js
@@ -0,0 +1,9 @@
+﻿//$('li a').hover(
+//function () {
+//    $(this).stop().fadeTo("slow", 0.4);
+//    $(this).stop().css("color", "white");
+//},
+//function () {
+//    $(this).stop().fadeTo('slow', 1);
+//    $(this).stop().css("color", "black");
+//});
